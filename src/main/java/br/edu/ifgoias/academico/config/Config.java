153a6f37daf12e1535ffbe05a223db7909f09faa
Henrique de Azara Tosta <henriqueazara3311@gmail.com>
2023-11-25 15:54:39 -0300

    First Commit

--- src/main/java/br/edu/ifgoias/academico/config/Config.java
+++ src/main/java/br/edu/ifgoias/academico/config/Config.java
@@ -14,11 +14,11 @@ import br.edu.ifgoias.academico.repositories.CursoRepository;
 @Configuration
 public class Config implements CommandLineRunner {
 
-	@Autowired
-	private CursoRepository cursoRep;
+	//@Autowired
+	//private CursoRepository cursoRep;
 	
-	@Autowired
-	private AlunoRepository alunoRep;
+	//@Autowired
+	//private AlunoRepository alunoRep;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -35,21 +35,21 @@ public class Config implements CommandLineRunner {
 
 				// cursoRep.deleteById(8);
 
-				System.out.println("Qtde Cursos: " + cursoRep.count());
+				//System.out.println("Qtde Cursos: " + cursoRep.count());
 
-				Aluno a1 = new Aluno(null, "Caroline", "Feminino", Date.valueOf("2000-04-24"));
+				//Aluno a1 = new Aluno(null, "Caroline", "Feminino", Date.valueOf("2000-04-24"));
 
-				Aluno a2 = new Aluno(null, "Isabelle", "Feminino", Date.valueOf("2000-12-28"));
+				//Aluno a2 = new Aluno(null, "Isabelle", "Feminino", Date.valueOf("2000-12-28"));
 
-				alunoRep.save(a1);
+				//alunoRep.save(a1);
 
-				alunoRep.save(a2);
+				//alunoRep.save(a2);
 
 				// alunoRep.deleteById(1);
 
 				// alunoRep.deleteById(2);
 
-				System.out.println("Qtde Alunos: " + alunoRep.count());
+				//System.out.println("Qtde Alunos: " + alunoRep.count());
 	}
 
 }
